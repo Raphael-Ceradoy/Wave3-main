f21f41f989e6c5d67612ac65d4da949485a10a43
Raphael-Ceradoy <rceradoy2004@gmail.com>
2024-03-26 21:05:08 -0400

    Cleaned up menus

--- src/com/wave3/main/Gamestate.java
+++ src/com/wave3/main/Gamestate.java
@@ -66,23 +66,52 @@ public class Gamestate {
 	
 	public void render(Graphics2D g) {
 		if(GAMESTATE == "start") {
-			g.setFont(new Font("Comic Sans MS", Font.PLAIN, 48));
+			/*g.setFont(new Font("Comic Sans MS", Font.PLAIN, 48));
 			g.setColor(Color.WHITE);
 			g.drawString("Previous Score: " + Gamestate.score, 100, 200);
-			g.drawString("Press Enter to Play!!!", 100, 300);
+			g.drawString("Press Enter to Play!!!", 100, 300);*/
+			Font fnt = new Font("arial", 1, 50);
+			Font fnt2 = new Font("arial", 1, 30);
+			g.setColor(Color.white);
+
+			g.setFont(fnt);
+			g.drawString("Menu", 375, 70);
+
+			g.setFont(fnt2);
+			g.drawRect(275, 165, 350, 100);
+			g.drawString("Previous Score: " + Gamestate.score, 300, 200);
+			g.drawString("Press Enter to Play!", 300, 250);
+
+			//g.drawRect(400, 250, 200, 64);
+			//g.drawString("Help", 450, 290);
+
+			//g.drawRect(400, 350, 200, 64);
+			//g.drawString("Quit", 450, 390);
 		}
 		else if(GAMESTATE == "game") {
 			handler.render(g);
 			hud.render(g);
 		}
 		else if(GAMESTATE == "pause") {
-			handler.render(g);
-			hud.render(g);
-			
+			//handler.render(g);
+			//hud.render(g);
+			/*
 			g.setFont(new Font("Comic Sans MS", Font.PLAIN, 48));
 			g.setColor(Color.WHITE);
 			g.drawString("Current Score: " + Gamestate.score, 100, 200);
-			g.drawString("Press Enter to Continue", 100, 300);
+			g.drawString("Press Enter to Continue", 100, 300); */
+			Font fnt = new Font("arial", 1, 50);
+			Font fnt2 = new Font("arial", 1, 30);
+			g.setColor(Color.white);
+
+			g.setFont(fnt);
+			g.drawString("Game Paused", 275, 70);
+
+			g.setFont(fnt2);
+			g.drawRect(265, 165, 360, 100);
+			g.drawString("Current Score: " + Gamestate.score, 270, 200);
+			g.drawString("Press Enter to Continue", 270, 250);
+
 		}
 	}
 	
